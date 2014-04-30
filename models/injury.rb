2da45d42ccb51255d4ffcc75c7a549c6bcca87a2
Eliza Brock <eliza@elizabrocksoftware.com>
2014-04-30 12:04:24 -0500

    First pass through detecting duplicate injuries.

--- models/injury.rb
+++ models/injury.rb
@@ -1,8 +1,10 @@
 class Injury
+  attr_reader :errors
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @errors = []
   end
 
   def self.count
@@ -11,18 +13,34 @@ class Injury
     result[0][0]
   end
 
+  def self.find_by_name(name)
+    statement = "Select * from injuries where name = '#{name}'"
+    execute_and_instantiate(statement)
+  end
+
   def self.last
     statement = "Select * from injuries order by id DESC limit(1)"
+    execute_and_instantiate(statement)
+  end
+
+  def save
+    if Injury.find_by_name(self.name)
+      @errors << "#{self.name} already exists."
+      false
+    else
+      statement = "Insert into injuries (name) values ('#{name}')"
+      Environment.database_connection.execute(statement)
+      true
+    end
+  end
+
+  private
+
+  def self.execute_and_instantiate(statement)
     result = Environment.database_connection.execute(statement)
     unless result.empty?
       name = result[0]["name"]
       Injury.new(name)
     end
-  end
-
-  def save
-    statement = "Insert into injuries (name) values ('#{name}')"
-    Environment.database_connection.execute(statement)
-    true
   end
 end
--- spec/injury_spec.rb
+++ spec/injury_spec.rb
@@ -20,6 +20,25 @@ describe Injury do
     end
   end
 
+  context ".find_by_name" do
+    context "with no injuries in the database" do
+      it "should return 0" do
+        Injury.find_by_name("Foo").should be_nil
+      end
+    end
+    context "with injury by that name in the database" do
+      before do
+        Injury.new("Foo").save
+        Injury.new("Bar").save
+        Injury.new("Baz").save
+        Injury.new("Grille").save
+      end
+      it "should return the injury with that name" do
+        Injury.find_by_name("Foo").name.should == "Foo"
+      end
+    end
+  end
+
   context ".last" do
     context "with no injuries in the database" do
       it "should return nil" do
@@ -47,18 +66,37 @@ describe Injury do
   end
 
   context "#save" do
-    let(:injury){ Injury.new("impalement, 1/2 - 2 inches diameter") }
     let(:result){ Environment.database_connection.execute("Select name from injuries") }
-    it "should return true" do
-      injury.save.should be_true
+    context "with a unique name" do
+      let(:injury){ Injury.new("impalement, 1/2 - 2 inches diameter") }
+      it "should return true" do
+        injury.save.should be_true
+      end
+      it "should only save one row to the database" do
+        injury.save
+        result.count.should == 1
+      end
+      it "should actually save it to the database" do
+        injury.save
+        result[0]["name"].should == "impalement, 1/2 - 2 inches diameter"
+      end
     end
-    it "should only save one row to the database" do
-      injury.save
-      result.count.should == 1
-    end
-    it "should actually save it to the database" do
-      injury.save
-      result[0]["name"].should == "impalement, 1/2 - 2 inches diameter"
+    context "with a duplicate name" do
+      let(:injury){ Injury.new("impalement, 1/2 inch diameter or smaller") }
+      before do
+        Injury.new("impalement, 1/2 inch diameter or smaller").save
+      end
+      it "should return false" do
+        injury.save.should be_false
+      end
+      it "should not save a new injury" do
+        injury.save
+        result.count.should == 1
+      end
+      it "should save the error messages" do
+        injury.save
+        injury.errors.first.should == "impalement, 1/2 inch diameter or smaller already exists."
+      end
     end
   end
 end
